cad0cab929fe7f1fd504eec0228b5358867209f7
Darthcooper <loganbarber1@outlook.com>
2025-04-07 10:01:07 -0600

    Added config system for easy customization

---
+++ configs/FroggerConfig.py
@@ -0,0 +1,18 @@
+# Native imports
+from typing import Tuple
+
+
+class BuildFroggerConfig:
+    env_name: str = "ALE/Frogger-v5"
+    render_mode : str = "rgb_array"
+    max_episode_steps : int = None
+    disable_env_checker : bool = None
+
+class PreprocessingFroggerConfig:
+    noop_max : int = 30
+    frame_skip : int = 1
+    screen_size: int | Tuple[int, int] = 84
+    terminal_on_life_loss : bool = False
+    grayscale_obs : bool = True
+    grayscale_newaxis : bool = True
+    scale_obs : bool = False
